d5c8c58f306e077099a06ea98a6d60341ed2c685
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2025-03-28 17:40:37 +0100

    Don't use cdloot_proc for Tidal Greatsword
    As the comment for the qpat specifies:
    
    > Must be called whenever a loot item with a cooldown that isn't
    > directly "used" (aka isn't an auto hotbarslot) is activated.

--- src/mod.zig
+++ src/mod.zig
@@ -3183,7 +3183,6 @@ fn transfiguredDepthSet() !void {
 
     trig.hotbarUsed(&.{.hb_self});
     qpat.hb_flash_item(.{});
-    qpat.hb_cdloot_proc(.{});
     qpat.hb_run_cooldown(.{});
     ttrg.players_opponent(.{});
     tset.strength_def(.{});
